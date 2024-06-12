24546d1356194874a473a591c78ba1d9378e3cb1
tavish <bhardwajtavishofficial@gmail.com>
2024-06-13 00:12:34 +0530

    Account Schema Created.

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
 const url = process.env.MONGODB_URL;
 
-
 mongoose.connect(url);
 
-const userSchema= new mongoose.Schema({
-    userName: String,
-    password: String,
-    firstName:String,
-    lastName: String,
+const userSchema = new mongoose.Schema({
+  userName: String,
+  password: String,
+  firstName: String,
+  lastName: String,
 });
 
-const User= mongoose.model("User", userSchema);
+const accountSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  balance: Number,
+});
 
-module.exports={
-    User,
-}
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
+
+module.exports = {
+  User,
+  Account
+};
